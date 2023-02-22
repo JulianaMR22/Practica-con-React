291ebdc1f6ec73bd9a65272b844aa2c9c1fc1787
JulianaMR22 <julianam2003@gmail.com>
2023-02-21 21:26:17 -0500

    Estilos con Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import TaskForm from "./assets/components/TaskForm.jsx";
 
 function App() {
   return (
-    <>
-      <TaskForm />
-      <Task />
-    </>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <Task />
+      </div>
+    </main>
   );
 }
 
